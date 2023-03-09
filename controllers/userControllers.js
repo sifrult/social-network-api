4863f7afca69e6e8a5484e171924472babb0ff09
Amy Snarr <amy.snarr16@gmail.com>
2023-03-08 21:25:59 -0700

    can delete friend

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -72,5 +72,17 @@ module.exports = {
     },
 
     // Delete friend from user
-    deleteFriend(req, res) {}
+    deleteFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id : req.params.userId},
+            { $pull: { friends: req.params.friendId }},
+            { runValidators: true, new: true}
+        )
+        .then((user) =>
+            !user
+            ? res.status(404).json({ message: 'No user with that ID' })
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    }
 }
